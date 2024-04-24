04b1be87544223cf1972b7163d47b300e6140c50
Elaidzha1940 <eli080293@gmail.com>
2024-04-25 01:38:24 +0300

    RickAndMorty

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: RickAndMorty
+//  File: ViewController.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 25.04.2024
+//
+//  */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan 
+    }
+}
+
