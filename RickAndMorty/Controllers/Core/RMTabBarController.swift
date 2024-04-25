c4d853d1139c09fc9ec631f2c0abfe82ff182e38
Elaidzha1940 <eli080293@gmail.com>
2024-04-25 05:34:57 +0300

    Update RMTabBarController

--- RickAndMorty/Controllers/Core/RMTabBarController.swift
+++ RickAndMorty/Controllers/Core/RMTabBarController.swift
@@ -9,11 +9,35 @@
 
 import UIKit
 
-class RMTabBarController: UITabBarController {
-
+final class RMTabBarController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan 
+        view.backgroundColor = .cyan
+        setUpTabs()
+    }
+    
+    private func setUpTabs() {
+        let charactersVC = RMCharacterViewController()
+        let locationsVC  = RMLocationViewController()
+        let episodesVC   = RMEpisodeViewController()
+        let settingsVC   = RMSettingsViewController()
+        
+        charactersVC.title = "Characters"
+        locationsVC.title  = "Locations"
+        episodesVC.title   = "Episodes"
+        settingsVC.title   = "Settings"
+        
+        let nav1 = UINavigationController(rootViewController: charactersVC)
+        let nav2 = UINavigationController(rootViewController: locationsVC)
+        let nav3 = UINavigationController(rootViewController: episodesVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        
+        setViewControllers(
+            [nav1, nav2, nav3, nav4],
+            animated: true
+        )
     }
 }
 
