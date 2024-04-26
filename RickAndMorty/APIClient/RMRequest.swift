ee86641858360585830e448ac0d5cf383775bcbe
Elaidzha1940 <eli080293@gmail.com>
2024-04-26 04:10:49 +0300

    Update APIClient / Added RMService + RMRequest

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,10 @@
+//  /*
+//
+//  Project: RickAndMorty
+//  File: RMRequest.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.04.2024
+//
+//  */
+
+import Foundation
---
+++ RickAndMorty/APIClient/RMService.swift
@@ -0,0 +1,14 @@
+//  /*
+//
+//  Project: RickAndMorty
+//  File: RMService.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.04.2024
+//
+//  */
+
+import Foundation
+
+final class RMService {
+    
+}
