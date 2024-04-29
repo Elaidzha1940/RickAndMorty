6b589faa1db721e48f639a8ced0f007915c2de49
Elaidzha1940 <eli080293@gmail.com>
2024-04-29 03:01:17 +0300

    Update RMRequest

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -16,10 +16,39 @@ final class RMRequest {
         static let baseUrl = "https://rickandmortyapi.com/api/location/3"
     }
     
-    let endpoint: RMEndpoint
-    let pathComponents: [String]
-    let queryParameters: [URLQueryItem]
+    private let endpoint: RMEndpoint
+    private let pathComponents: [String]
+    private let queryParameters: [URLQueryItem]
     
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach ({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else { return nil }
+                return "/\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    private var url: URL? {
+        return nil
+    }
+    
+    // MARK: - Public
     public init(endpoint: RMEndpoint,
                 pathComponents: [String] = [],
                 queryParameters: [URLQueryItem] = []
