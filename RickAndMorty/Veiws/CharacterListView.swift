da480a92b2e97b976bf9c894b5613adbc60cb3ef
Elaidzha1940 <eli080293@gmail.com>
2024-05-02 23:31:52 +0500

    Created CharacterListView

--- RickAndMorty/Veiws/CharacterListView.swift
+++ RickAndMorty/Veiws/CharacterListView.swift
@@ -11,14 +11,46 @@ import UIKit
 
 /// View that handels showing list of characters, loader, etc.
 final class CharacterListView: UIView {
-
+    
+    private let viewModel = CharacterListViewModel()
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .medium)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(UICollectionViewCell.self,
+                                forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
+    // MARK: - Init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .systemCyan
+        addSubview(spinner)
+        
+        addConstraint()
+        spinner.startAnimating()
+        viewModel.fetchCharacters()
     }
     
     required init?(coder: NSCoder) {
         fatalError("Unsupported")
+    }
+    
+    private func addConstraint() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
     }
 }
