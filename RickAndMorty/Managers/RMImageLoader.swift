6a6eb095b0e8989d4a798988e168dcc8f7d2c668
Elaidzha1940 <eli080293@gmail.com>
2024-05-18 12:26:04 +0300

    Update RMImageLoader

--- RickAndMorty/Managers/RMImageLoader.swift
+++ RickAndMorty/Managers/RMImageLoader.swift
@@ -16,13 +16,26 @@ final class RMImageLoader {
     
     private init() {}
     
-   public func downloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> ()) {
-        let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+    /// Get image content with URL
+    /// - Parameters:
+    ///   - url: Source url
+    ///   - completion: Callback
+    public func downloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> ()) {
+        let key = url.absoluteString as NSString
+        if let data = imageDataCache.object(forKey: key) {
+            print("Reading from cache: \(key)")
+            completion(.success(data as Data)) // NSData == Data | NSString == String
+            return
+        }
+            
+            let request = URLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
             guard let data = data, error == nil else {
                 completion(.failure(error ?? URLError(.badServerResponse)))
                 return
             }
+            let value = data as NSData
+            self?.imageDataCache.setObject(value, forKey: key)
             completion(.success(data))
         }
         task.resume()
