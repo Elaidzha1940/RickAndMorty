9dc1c9fa96949ba1d3decdd4ed1aa8e6f04a9822
Elaidzha1940 <eli080293@gmail.com>
2024-05-22 15:35:02 +0300

    Added RMCharacterPhotoCollectionViewCellViewModel / RMCharacterInfoCollectionViewCellViewModel / RMCharacterEpisodeCollectionViewCellViewModel

---
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
@@ -0,0 +1,14 @@
+//  /*
+//
+//  Project: RickAndMorty
+//  File: RMCharacterEpisodeCollectionViewCellViewModel.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 22.05.2024
+//
+//  */
+
+import Foundation
+
+final class RMCharacterEpisodeCollectionViewCellViewModel {
+    init() {}
+}
---
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterPhotoCollectionViewCellViewModel.swift
@@ -0,0 +1,14 @@
+//  /*
+//
+//  Project: RickAndMorty
+//  File: RMCharacterPhotoCollectionViewCellViewModel.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 22.05.2024
+//
+//  */
+
+import Foundation
+
+final class RMCharacterPhotoCollectionViewCellViewModel {
+    init() {}
+}
