18127e5345571fdc5e00f931892d29e2a603aa98
Elaidzha1940 <eli080293@gmail.com>
2024-05-30 15:36:00 +0300

    Update RMEpisodeViewController

--- RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
+++ RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
@@ -10,11 +10,33 @@
 import UIKit
 
 /// Controller to show and search for Episode
-final class RMEpisodeViewController: UIViewController {
-
+final class RMEpisodeViewController: UIViewController, RMEpisodeListViewDelegate {
+    private let episodeListView = RMEpisodeListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        title = "Episodes"
+        title = "Episode"
+        setUpView()
+    }
+    
+    private func setUpView() {
+        episodeListView.delegate = self
+        view.addSubview(episodeListView)
+        NSLayoutConstraint.activate([
+            episodeListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            episodeListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            episodeListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            episodeListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    // MARK: - RMEpisodeListViewDelegate
+    
+    func rmEpisodeListView(_ characterListView: RMEpisodeListView, didSelectEpisode episode: RMEpisode) {
+        // Open detail controller for that episode
+        let detailVC = RMEpisodeDetailViewController(url: URL(string: episode.url))
+        detailVC.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(detailVC, animated: true)
     }
 }
