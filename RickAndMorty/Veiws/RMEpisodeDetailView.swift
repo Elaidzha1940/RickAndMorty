29c15753bdfbf8cdac345899c217be570ce50117
Elaidzha1940 <eli080293@gmail.com>
2024-05-30 13:45:07 +0300

    Added RMEpisodeDetailView / RMEpisodeDetailViewModel

---
+++ RickAndMorty/Veiws/RMEpisodeDetailView.swift
@@ -0,0 +1,15 @@
+//  /*
+//
+//  Project: RickAndMorty
+//  File: RMEpisodeDetailView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 30.05.2024
+//
+//  */
+
+import UIKit
+
+class RMEpisodeDetailView: UIView {
+
+
+}
---
+++ RickAndMorty/ViewModels/RMEpisodeDetailViewModel.swift
@@ -0,0 +1,20 @@
+//  /*
+//
+//  Project: RickAndMorty
+//  File: RMEpisodeDetailViewModel.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 30.05.2024
+//
+//  */
+
+import UIKit
+
+class RMEpisodeDetailViewModel {
+    private let endpointUrl: URL?
+    
+    // MARK: - Init
+    
+    init(endpointUrl: URL?) {
+        self.endpointUrl = endpointUrl
+    }
+}
