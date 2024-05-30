e40f00bbc0a17a06d91f74114c2ca26c27ba5bef
Elaidzha1940 <eli080293@gmail.com>
2024-05-30 13:34:27 +0300

    Updated RMService

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -35,13 +35,24 @@ final class RMService {
         expecting type: T.Type,
         completion: @escaping (Result<T, Error>) -> Void
     ) {
-        
+        if let cachedData = cacheManager.chachedResponse(for: request.endpoint, url: request.url) {
+            print("Using cache API")
+
+            do {
+                let result = try JSONDecoder().decode(type.self, from: cachedData)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+            return
+        }
         
         guard let urlRequest = self.request(from: request) else {
             completion(.failure(RMServiceError.failedToCreateRequest))
             return
         }
-        let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] data, _, error in
             guard let data = data, error == nil else {
                 completion(.failure(error ?? RMServiceError.failedToGetData))
                 return
@@ -50,8 +61,8 @@ final class RMService {
             // Decode response
             do {
                 let result = try JSONDecoder().decode(type.self, from: data)
+                self?.cacheManager.setCache(for: request.endpoint, url: request.url, data: data)
                 completion(.success(result))
-                print(String(describing: result))
             } catch {
                 completion(.failure(error))
             }
