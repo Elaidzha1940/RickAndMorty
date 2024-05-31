f9082cf69c23c4bc6c3fec43ecb8b72438bd27a0
Elaidzha1940 <eli080293@gmail.com>
2024-05-31 10:52:40 +0300

    Update RMEpisodeListViewModelDelegate / RMEpisodeListViewModel

--- RickAndMorty/Veiws/Episode/RMEpisodeListView .swift
+++ RickAndMorty/Veiws/Episode/RMEpisodeListView .swift
@@ -97,6 +97,6 @@ extension RMEpisodeListView: RMEpisodeListViewModelDelegate {
     }
     
     func didSelectEpisode(_ episode: RMEpisode) {
-        //        delegate?.rmCharacterListView(self, didSelectCharacter: character)
+        delegate?.rmEpisodeListView(self, didSelectEpisode: episode)
     }
 }
