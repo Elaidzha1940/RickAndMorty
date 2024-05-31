6f75a15f630762898cd247215605521a6371aed6
Elaidzha1940 <eli080293@gmail.com>
2024-05-31 11:52:43 +0300

    Update RMCharacterEpisodeCollectionViewCell

--- RickAndMorty/Veiws/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
+++ RickAndMorty/Veiws/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
@@ -38,15 +38,18 @@ class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .tertiarySystemBackground
-        contentView.layer.cornerRadius = 10
-        contentView.layer.borderWidth = 2
-        contentView.layer.borderColor = UIColor.systemMint.cgColor
+        setUpLayer()
         contentView.addSubviews(seasonLabel, nameLabel, airDateLabel)
         setUpConstraints()
     }
    
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    private func setUpLayer() {
+        contentView.layer.cornerRadius = 10
+        contentView.layer.borderWidth = 2
     }
     
     private func setUpConstraints() {
@@ -83,5 +86,6 @@ class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
             self?.airDateLabel.text = "Aired on "+data.air_date
         }
         viewModel.fetchEpisode()
+        contentView.layer.borderColor = viewModel.borderColor.cgColor
     }
 }
--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
@@ -7,7 +7,7 @@
 //
 //  */
 
-import Foundation
+import UIKit
 
 protocol RMEpisodeDataRender {
     var name:     String { get }
@@ -21,6 +21,8 @@ final class RMCharacterEpisodeCollectionViewCellViewModel: Hashable, Equatable {
     private var isFetching = false
     private var dataBlock: ((RMEpisodeDataRender) -> ())?
     
+    public let borderColor: UIColor
+    
     private var episode: RMEpisode? {
         didSet {
             guard let model = episode else {
@@ -32,8 +34,9 @@ final class RMCharacterEpisodeCollectionViewCellViewModel: Hashable, Equatable {
     
     // MARK: - Init
     
-    init(episodeDataUrl: URL?) {
+    init(episodeDataUrl: URL?, borderColor: UIColor = .systemGray) {
         self.episodeDataUrl = episodeDataUrl
+        self.borderColor = borderColor
     }
     
     // MARK: - Public
