fd5a4dfb8a32790bf863ecbec2c3540f93033697
Elaidzha1940 <eli080293@gmail.com>
2024-06-05 16:07:38 +0300

    Update Episode / RMEpisodeInfoCollectionViewCell

--- RickAndMorty/Veiws/EpisodeDetails/RMEpisodeInfoCollectionViewCell.swift
+++ RickAndMorty/Veiws/EpisodeDetails/RMEpisodeInfoCollectionViewCell.swift
@@ -12,20 +12,67 @@ import UIKit
 final class RMEpisodeInfoCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "RMEpisodeInfoCollectionViewCell"
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.textAlignment = .right
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .systemBrown
+        contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubviews(titleLabel, valueLabel)
+        setUpLayer()
+        addConstraint()
     }
     
     required init?(coder: NSCoder) {
         fatalError()
     }
     
+    private func setUpLayer() {
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.secondaryLabel.cgColor
+    }
+    
+    private func addConstraint() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
+            titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 4),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4),
+            
+            valueLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
+            valueLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -4),
+            valueLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4),
+            
+            titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.47),
+            valueLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.47),
+        ])
+        
+        titleLabel.backgroundColor = .yellow
+        valueLabel.backgroundColor = .systemMint
+    }
+    
     override func prepareForReuse() {
         super.prepareForReuse()
+        titleLabel.text = nil
+        valueLabel.text = nil
     }
     
     func configure(with viewModel: RMEpisodeInfoCollectionViewCellViewModel) {
-        
+        titleLabel.text = viewModel.title
+        valueLabel.text = viewModel.value
     }
 }
