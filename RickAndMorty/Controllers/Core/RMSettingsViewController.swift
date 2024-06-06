a2a73031a3747d8bb0775899e6f24827c2b3702b
Elaidzha1940 <eli080293@gmail.com>
2024-06-06 15:45:12 +0300

    Update RMSettingsViewController

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -7,21 +7,35 @@
 //
 //  */
 
+import SwiftUI
 import UIKit
 
 /// Controller to show various options and settings
 final class RMSettingsViewController: UIViewController {
     
-    private let viewModel = RMSettingsViewModel(
-    cellViewModel: RMSettingsOption.allCases.compactMap({
-        return RMSettingsCellViewModel(type: $0)
-    }))
+    private let settingsSwiftUIController = UIHostingController(
+        rootView: RMSettingsView(viewModel: RMSettingsViewModel(
+            cellViewModel: RMSettingsOption.allCases.compactMap({
+                return RMSettingsCellViewModel(type: $0)
+            }))))
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Settings"
+        addSwiftUIController()
+    }
+    
+    private func addSwiftUIController() {
+        addChild(settingsSwiftUIController)
+        settingsSwiftUIController.didMove(toParent: self)
+        
+        view.addSubview(settingsSwiftUIController.view)
+        settingsSwiftUIController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+        
+        ])
     }
 }
 
