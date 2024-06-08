dd4b8891da6ef5431df2d891e622a2926fcbef49
Elaidzha1940 <eli080293@gmail.com>
2024-06-08 12:12:29 +0300

    Added RMLocationView

---
+++ RickAndMorty/Veiws/Location/RMLocationView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: RickAndMorty
+//  File: RMLocationView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 08.06.2024
+//
+//  */
+
+import UIKit
+
+final class RMLocationView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+}
