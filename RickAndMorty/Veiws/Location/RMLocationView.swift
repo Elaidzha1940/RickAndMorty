07a0bc8c7714ee8b5d1ab39eea3fc7fff3a63aca
Elaidzha1940 <eli080293@gmail.com>
2024-06-08 12:58:54 +0300

    Update RMLocationViewController / RMLocationView

--- RickAndMorty/Veiws/Location/RMLocationView.swift
+++ RickAndMorty/Veiws/Location/RMLocationView.swift
@@ -11,13 +11,47 @@ import UIKit
 
 final class RMLocationView: UIView {
     
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.alpha = 0
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
+    // MARK: - Init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .systemPink
+        addSubviews(tableView, spinner)
+        spinner.startAnimating()
+        addConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError()
     }
     
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
 }
