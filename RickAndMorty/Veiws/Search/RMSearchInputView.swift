23830019aea3d0447ed291d1486ae0a36e60cc98
Elaidzha1940 <eli080293@gmail.com>
2024-06-20 19:51:35 +0300

    Update RMSearchInputView

--- RickAndMorty/Veiws/Search/RMSearchInputView.swift
+++ RickAndMorty/Veiws/Search/RMSearchInputView.swift
@@ -11,10 +11,20 @@ import UIKit
 
 final class RMSearchInputView: UIView {
     
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search"
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
+    // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemTeal
+        addSubviews(searchBar)
+        addConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -22,7 +32,12 @@ final class RMSearchInputView: UIView {
     }
     
     private func addConstraints() {
-        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: topAnchor, constant: 3),
+            searchBar.leftAnchor.constraint(equalTo: leftAnchor, constant: 5),
+            searchBar.rightAnchor.constraint(equalTo: rightAnchor, constant: -5),
+            searchBar.heightAnchor.constraint(equalToConstant: 55)
+        ])
     }
     
     public func configure(with viewModel: RMSearchInputView) {
