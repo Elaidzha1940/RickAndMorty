d459fe7b5e84e1f1940eb878eafd8aea9953b00e
Elaidzha1940 <eli080293@gmail.com>
2024-06-20 19:34:55 +0300

    Update RMSearchInputView

--- RickAndMorty/Veiws/Search/RMSearchInputView.swift
+++ RickAndMorty/Veiws/Search/RMSearchInputView.swift
@@ -11,4 +11,20 @@ import UIKit
 
 final class RMSearchInputView: UIView {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func addConstraints() {
+        
+    }
+    
+    public func configure(with viewModel: RMSearchInputView) {
+        
+    }
 }
