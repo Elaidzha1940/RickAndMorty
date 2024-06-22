1a2326ab1e6d9f291524b32aef4ec5e8175bc60b
Elaidzha1940 <eli080293@gmail.com>
2024-06-22 17:32:50 +0300

    Update RMSearchInputView

--- RickAndMorty/Veiws/Search/RMSearchInputView.swift
+++ RickAndMorty/Veiws/Search/RMSearchInputView.swift
@@ -32,7 +32,6 @@ final class RMSearchInputView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .systemTeal
         addSubviews(searchBar)
         addConstraints()
     }
@@ -40,6 +39,8 @@ final class RMSearchInputView: UIView {
     required init?(coder: NSCoder) {
         fatalError()
     }
+    
+    // MARK: - Private
     
     private func addConstraints() {
         NSLayoutConstraint.activate([
@@ -50,12 +51,51 @@ final class RMSearchInputView: UIView {
         ])
     }
     
+    private func createOptionStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: rightAnchor),
+            stackView.topAnchor.constraint(equalTo: bottomAnchor),
+        ])
+        
+        return stackView
+    }
+    
     private func createOptionSelectionViews(options: [RMSearchInputViewModel.DynamicOption]) {
-        for option in options {
-            print(option.rawValue)
+        let stackView = createOptionStackView()
+        
+        for x in 0..<options.count {
+            let option = options[x]
+            let button = UIButton()
+            button.setTitle(option.rawValue, for: .normal)
+            button.backgroundColor = .systemCyan
+            button.setTitleColor(.label, for: .normal)
+            button.addTarget(self, action: #selector(didTapButton(_ :)), for: .touchUpInside)
+            button.tag = x
+            stackView.addArrangedSubview(button)
         }
     }
     
+    @objc
+    private func didTapButton(_ sender: UIButton) {
+        guard let options = viewModel?.options else {
+            return
+        }
+        let tag = sender.tag
+        let selected = options[tag]
+        
+        print("Did tap \(selected.rawValue)")
+    }
+    
+    // MARK: - Public
     public func configure(with viewModel: RMSearchInputViewModel) {
         searchBar.placeholder = viewModel.searchPlaceholderText
         self.viewModel = viewModel
