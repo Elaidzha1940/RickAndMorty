a2f45a278fe1fce8d372936152f0b878874ee01d
Elaidzha1940 <eli080293@gmail.com>
2024-07-10 15:24:56 +0300

    Updated RMSearchResultsView

--- RickAndMorty/Veiws/Search/RMSearchResultsView.swift
+++ RickAndMorty/Veiws/Search/RMSearchResultsView.swift
@@ -198,3 +198,33 @@ extension RMSearchResultsView: UICollectionViewDelegate, UICollectionViewDataSou
         return CGSize(width: width, height: 100)
     }
 }
+
+extension RMLocationView: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        guard let viewModel = viewModel,
+              !viewModel.cellViewModels.isEmpty,
+              viewModel.shouldShowLoadMoreIndicator,
+              !viewModel.isLoadingMoreLocations else {
+            return
+        }
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { [weak self] t in
+            let offset = scrollView.contentOffset.y
+            let totalContentHeight = scrollView.contentSize.height
+            let totalScrollViewFixedHeight = scrollView.frame.size.height
+            
+            if offset >= (totalContentHeight - totalScrollViewFixedHeight - 120) {
+                DispatchQueue.main.async {
+                    self?.showLoadingIndicator()
+                }
+                viewModel.fetchAdditionalLocations()
+            }
+            t.invalidate()
+        }
+    }
+    
+    private func showLoadingIndicator() {
+        let footer = RMTableLoadingFooterView(frame:  CGRect(x: 0, y: 0, width: frame.size.width, height: 100))
+        tableView.tableFooterView = footer
+    }
+}
